1654a490bdb00e82c47e9da52e78c3d034711014
Tanishq Gadkari <gadkaritanishq@gmail.com>
2020-03-11 02:08:48 -0700

    added comments to pd.py

--- pd.py
+++ pd.py
@@ -1,17 +1,22 @@
 import pandas as pd
 import pprint
 
-data_set = pd.read_csv(r'data/2019_nCoV_data.csv')
-print(data_set.head())
-print(data_set.columns)
+data_set = pd.read_csv(r'data/2019_nCoV_data.csv') # load the data set
+print(data_set.head()) # prints the first few data set 
+print(data_set.columns) # prints the column name values e.g. name, email, etc
 
-countries = data_set['Country'].tolist()
-
-
-unique_countries = list(dict.fromkeys(countries))
-pprint.pprint(unique_countries, indent = 4)
+countries = data_set['Country'].tolist() # convert the dataframe of Country into a list
 
 
+# basically uses the list value, which hold redundancy, and makes a dict aka hash map. Hash
+# maps only allow unique key values which means it'll get rid of all the redundant countries
+# and we're left with unique countries. Now we just convert the dict back to a list since 
+# that was our original goal
+unique_countries = list(dict.fromkeys(countries)) 
+pprint.pprint(unique_countries, indent = 4) #show the unique countries
+
+
+# nested loop to print all the total number of cases that happened in each country.
 for country in unique_countries:
     total = 0
     for curr_country in countries:
